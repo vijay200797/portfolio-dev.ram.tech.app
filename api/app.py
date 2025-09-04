c45430aa9b3899f12c3a0031fa6b4fc076c8f0cc
vijay200797 <vijay200797@gmail.com>
2025-09-04 12:36:36 +0530

    Code Commit

--- api/app.py
+++ api/app.py
@@ -65,4 +65,5 @@ def submit():
         return "OK"
 
 if __name__ == "__main__":
-    app.run(host='https://portfolio-dev-ram-tech-app.vercel.app', port=5000, debug= True)
+    # app.run(host='127.0.0.1', port=5000, debug= True)
+    app.run(debug= False)
