7f75d80fbf11b6c230129c50c532d20f651f9f56
vijay200797 <vijay200797@gmail.com>
2026-09-19 11:47:46 +0530

    code update

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import smtplib, ssl
 import json
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 #app = Flask(__name__)
